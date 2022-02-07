767cf48123890fb3802feacf98f1004d7210e404
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-02-07 09:40:35 +0800

    Initial Commit

---
+++ PicsMac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PicsMac
+//
+//  Created by Runhua Huang on 2022/2/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PicsMac/PicsMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicsMacApp.swift
+//  PicsMac
+//
+//  Created by Runhua Huang on 2022/2/7.
+//
+
+import SwiftUI
+
+@main
+struct PicsMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
