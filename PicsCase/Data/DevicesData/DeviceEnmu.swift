efaa98f8283602197686d8d90afb2dbe6b172a5d
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-01-15 09:57:23 +0800

    bug fixes

--- PicsCase/Data/DevicesData/DeviceEnmu.swift
+++ PicsCase/Data/DevicesData/DeviceEnmu.swift
@@ -9,37 +9,37 @@ import Foundation
 
 enum DeviceCase: String, CaseIterable, Identifiable, Equatable {
     
-    case AppleWatch44mmSeries6 = "Apple Watch 44mm Series 6"
-    case AppleWatch45mmSeries7 = "Apple Watch 45mm Series 7"
-    case iPhone11 = "iPhone 11"
-    case iPhone11Pro = "iPhone 11 Pro"
-    case iPhone11ProMax = "iPhone 11 Pro Max"
-    case iPhone12Mini = "iPhone 12 Mini"
-    case iPhone12 = "iPhone 12"
-    case iPhone12Pro = "iPhone 12 Pro"
-    case iPhone12ProMax = "iPhone 12 Pro Max"
-    case iPhone13Mini = "iPhone 13 Mini"
-    case iPhone13 = "iPhone 13"
-    case iPhone13Pro = "iPhone 13 Pro"
-    case iPhone13ProMax = "iPhone 13 Pro Max"
-    case iPadLandscape = "iPad (Landscape)"
-    case iPadPortrait = "iPad (Portrait)"
-    case iPadminiLandscape = "iPad mini (Landscape)"
-    case iPadminiPortrait = "iPad mini (Portrait)"
-    case iPadAirLandscape = "iPad Air (Landscape)"
-    case iPadAirPortrait = "iPad Air (Portrait)"
-    case iPadPro11Landscape = "iPad Pro 11-inch (Landscape)"
-    case iPadPro11Portrait = "iPad Pro 11-inch (Portrait)"
-    case iPadPro12Landscape = "iPad Pro 12-inch (Landscape)"
-    case iPadPro12Portrait = "iPad Pro 12-inch (Portrait)"
-    case MacBookAir13 = "MacBook Air 13-inch"
-    case iMac24 = "iMac 24-inch"
-    case iMac27 = "iMac 27-inch"
-    case MacBookPro15Gen4 = "MacBook Pro 15-inch (Gen4)"
-    case MacBookPro16Gen4 = "MacBook Pro 16-inch (Gen4)"
-    case MacBookPro14Gen5 = "MacBook Pro 14-inch (Gen5)"
-    case MacBookPro16Gen5 = "MacBook Pro 16-inch (Gen5)"
-    case AppleTV4K = "Apple TV 4K"
+    case AppleWatch44mmSeries6
+    case AppleWatch45mmSeries7
+    case iPhone11
+    case iPhone11Pro
+    case iPhone11ProMax
+    case iPhone12Mini
+    case iPhone12
+    case iPhone12Pro
+    case iPhone12ProMax
+    case iPhone13Mini
+    case iPhone13
+    case iPhone13Pro
+    case iPhone13ProMax
+    case iPadLandscape
+    case iPadPortrait
+    case iPadminiLandscape
+    case iPadminiPortrait
+    case iPadAirLandscape
+    case iPadAirPortrait
+    case iPadPro11Landscape
+    case iPadPro11Portrait
+    case iPadPro12Landscape
+    case iPadPro12Portrait
+    case MacBookAir13
+    case iMac24
+    case iMac27
+    case MacBookPro15Gen4
+    case MacBookPro16Gen4
+    case MacBookPro14Gen5
+    case MacBookPro16Gen5
+    case AppleTV4K
 
     var id: String { self.rawValue }
     var description: String {
